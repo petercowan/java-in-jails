1c82e6938923c0fc460191d2f11627550ef23680
peter cowan <peter@petercowan.com>
2011-12-01 17:14:25 -0800

    add demo files

--- demo/java/org/jails/demo/ModelBean.java
+++ demo/java/org/jails/demo/ModelBean.java
@@ -1,4 +1,47 @@
 package org.jails.demo;
 
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
 public class ModelBean {
+	@NotBlank
+	@Size(min = 2, max = 25)
+	private String name;
+
+	@Max(value = 12345)
+	@Min(value = 0)
+	private Integer size;
+
+	@NotBlank
+	@Past
+	private Date dateCreated;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getSize() {
+		return size;
+	}
+
+	public void setSize(Integer size) {
+		this.size = size;
+	}
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
 }
--- demo/java/org/jails/demo/SimpleFormServlet.java
+++ demo/java/org/jails/demo/SimpleFormServlet.java
@@ -1,4 +1,41 @@
 package org.jails.demo;
 
-public class SimpleFormServlet {
+import org.jails.form.SimpleForm;
+import org.jails.form.controller.SimpleFormRouter;
+import org.jails.validation.SimpleValidator;
+import org.jails.validation.ValidationException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SimpleFormServlet extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		SimpleValidator validator = new SimpleValidator();
+
+		SimpleForm<ModelBean> simpleForm = SimpleForm.validateAs(ModelBean.class);
+		SimpleFormRouter simpleFormRequest = new SimpleFormRouter("model_bean");
+
+		if (simpleFormRequest.isSubmitted(request)) {
+			try {
+				ModelBean bean = validator.validate(ModelBean.class, request.getParameterMap());
+				//do something with valid bean
+
+				request.setAttribute("model_bean", bean);
+				//now display bean
+			} catch (ValidationException e) {
+				simpleForm.setErrors(e.getErrorFields());
+				//back to form page
+			} catch (Exception e) {
+				//handle error
+			}
+		} else {
+			simpleForm.inRequest(request);
+			//forward to formPage
+		}
+
+	}
 }
